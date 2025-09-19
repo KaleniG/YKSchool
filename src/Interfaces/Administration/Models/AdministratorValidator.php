8bbe24feb17d8f90633a5c2b382cdc576f282309
KaleniG <151833811+KaleniG@users.noreply.github.com>
2025-09-19 10:16:30 +0200

    Stupid error, how did it even work before???

--- src/Interfaces/Administration/Models/AdministratorValidator.php
+++ src/Interfaces/Administration/Models/AdministratorValidator.php
@@ -2,12 +2,13 @@
 
 namespace App\Interfaces\Administration\Models;
 
+use App\Config\LogManager;
 use App\Interfaces\Common\Models\UserValidator;
 
 class AdministratorValidator extends UserValidator
 {
   public function validateAdministrator($name, $surname)
   {
-    $this->validate("administrators", $name, $surname);
+    return $this->validate("administrators", $name, $surname);
   }
 }
